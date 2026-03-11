ba8718a5df12c7c921b691236bade6cfa63fde72
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2026-03-11 17:11:17 +0100

    ajout commentaire + gitignore

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -8,27 +8,42 @@ class GildedRose {
     }
 
     public void updateQuality() {
+        // pour chaque item
         for (int i = 0; i < items.length; i++) {
+            // si l'item n'est pas Aged Brie et le Backstage passes
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                // si la qualité est plus grande que 0
                 if (items[i].quality > 0) {
+                    // si l'item n'est pas Sulfuras
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                        // on retire 1 à sa qualité
                         items[i].quality = items[i].quality - 1;
                     }
                 }
+            // sinon
             } else {
+                // si la qualité de l'item est en dessous de 50
                 if (items[i].quality < 50) {
+                    // on augmente sa qualité de 1
                     items[i].quality = items[i].quality + 1;
 
+                    // si le nom de l'item est le Backstage passes
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        // si son prix est en dessous de 11
                         if (items[i].sellIn < 11) {
+                            // si sa qualité est en dessous de 50
                             if (items[i].quality < 50) {
+                                // on augmente sa qualité de 1
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
+                        // si son prix de vente est en dessous de 6
                         if (items[i].sellIn < 6) {
+                            // si sa qualité est en dessous de 50
                             if (items[i].quality < 50) {
+                                // on augmente sa qualité de 1
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
@@ -36,23 +51,36 @@ class GildedRose {
                 }
             }
 
+            // si l'item n'est pas sulfuras
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                // on réduit son prix de 1
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
+            // si le prix de l'item est en dessous de 0
             if (items[i].sellIn < 0) {
+                // si l'item n'est pas Aged Brie
                 if (!items[i].name.equals("Aged Brie")) {
+                    // si l'item n'est pas le Backstage passes
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        // si l'item à une qualité supérieur à 0
                         if (items[i].quality > 0) {
+                            // si l'item n'est pas Sulfuras
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                                // on réduit sa qualité de 1
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
+                    // sinon
                     } else {
+                        // met la qualité à 0
                         items[i].quality = items[i].quality - items[i].quality;
                     }
+                // sinon
                 } else {
+                    // si la qualité est en desous de 50
                     if (items[i].quality < 50) {
+                        // augmenter la qualité de 1
                         items[i].quality = items[i].quality + 1;
                     }
                 }
