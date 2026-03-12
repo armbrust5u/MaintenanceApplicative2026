57b2eb2fd156da6ea81f737224478390f9008841
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2026-03-12 10:21:56 +0100

    refact: separation de la methode en plusieurs methode avec un switch

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -15,106 +15,79 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             switch (this.items[i].name) {
                 case "Aged Brie":
-                    updateAgedBrie();
+                    updateAgedBrie(items[i]);
                     break;
                 case "Backstage passes to a TAFKAL80ETC concert":
-                    updateBackstage();
+                    updateBackstage(items[i]);
                     break;
                 case "Sulfuras, Hand of Ragnaros":
-                    updateSulfuras();
                     break;
                 default:
+                    updateQualityDefault(items[i]);
                     break;
             }
         }
     }
 
-    private void updateSulfuras() {
-    }
+    private void updateAgedBrie(Item item) {
+        // si la qualité de l'item est en dessous de 50
+        if (item.quality < QUALITE_MAX) {
+            // on augmente sa qualité de 1
+            item.quality = item.quality + 1;
+        }
 
-    private void updateBackstage() {
-    }
+        item.sellIn = item.sellIn - 1;
 
-    private void updateAgedBrie() {
-    }
-
-
-    public void updateQuality2() {
-        for (int i = 0; i < items.length; i++) {
-            // si l'item n'est pas Aged Brie ni le Backstage passes
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                // si la qualité est plus grande que 0
-                if (items[i].quality > 0) {
-                    // si l'item n'est pas Sulfuras
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        // on retire 1 à sa qualité
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-                // sinon
-            } else {
-                // si la qualité de l'item est en dessous de 50
-                if (items[i].quality < QUALITE_MAX) {
-                    // on augmente sa qualité de 1
-                    items[i].quality = items[i].quality + 1;
-
-                    // si le nom de l'item est le Backstage passes
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        // si son prix est en dessous de 11
-                        if (items[i].sellIn < PRIX_MAX1) {
-                            // si sa qualité est en dessous de 50
-                            if (items[i].quality < QUALITE_MAX) {
-                                // on augmente sa qualité de 1
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        // si son prix est en dessous de 6
-                        if (items[i].sellIn < PRIX_MAX2) {
-                            // si sa qualité est en dessous de 50
-                            if (items[i].quality < QUALITE_MAX) {
-                                // on augmente sa qualité de 1
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+        // si le prix de l'item est en dessous de 0
+        if (item.sellIn < 0) {
+            // si la qualité est en desous de 50
+            if (item.quality < QUALITE_MAX) {
+                // augmenter la qualité de 1
+                item.quality = item.quality + 1;
             }
+        }
+    }
 
-            // si l'item n'est pas sulfuras
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                // on réduit son prix de 1
-                items[i].sellIn = items[i].sellIn - 1;
+
+    private void updateBackstage(Item item) {
+        // si la qualité de l'item est en dessous de 50
+        if (item.quality < QUALITE_MAX) {
+            // on augmente sa qualité de 1
+            item.quality = item.quality + 1;
+        }
+
+        if (item.sellIn < PRIX_MAX1) {
+            if (item.quality < QUALITE_MAX) {
+                item.quality = item.quality + 1;
             }
+        }
 
-            // si le prix de l'item est en dessous de 0
-            if (items[i].sellIn < 0) {
-                // si l'item n'est pas Aged Brie
-                if (!items[i].name.equals("Aged Brie")) {
-                    // si l'item n'est pas le Backstage passes
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        // si l'item à une qualité supérieur à 0
-                        if (items[i].quality > 0) {
-                            // si l'item n'est pas Sulfuras
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                // on réduit sa qualité de 1
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                        // sinon
-                    } else {
-                        // met la qualité à 0
-                        items[i].quality = 0;
-                    }
-                    // sinon
-                } else {
-                    // si la qualité est en desous de 50
-                    if (items[i].quality < QUALITE_MAX) {
-                        // augmenter la qualité de 1
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+        if (item.sellIn < PRIX_MAX2) {
+            if (item.quality < QUALITE_MAX) {
+                item.quality = item.quality + 1;
+            }
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        // si le prix de l'item est en dessous de 0
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void updateQualityDefault(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        // si le prix de l'item est en dessous de 0
+        if (item.sellIn < 0) {
+            // si l'item à une qualité supérieur à 0
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
         }
     }
