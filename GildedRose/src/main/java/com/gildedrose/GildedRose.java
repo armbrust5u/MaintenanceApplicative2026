5963c3089b72ccd61c643d05e8af96a4911abc3a
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2026-03-12 10:23:58 +0100

    meilleur visibilite

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -33,17 +33,17 @@ class GildedRose {
         // si la qualité de l'item est en dessous de 50
         if (item.quality < QUALITE_MAX) {
             // on augmente sa qualité de 1
-            item.quality = item.quality + 1;
+            item.quality++;
         }
 
-        item.sellIn = item.sellIn - 1;
+        item.sellIn--;
 
         // si le prix de l'item est en dessous de 0
         if (item.sellIn < 0) {
             // si la qualité est en desous de 50
             if (item.quality < QUALITE_MAX) {
                 // augmenter la qualité de 1
-                item.quality = item.quality + 1;
+                item.quality++;
             }
         }
     }
@@ -53,18 +53,18 @@ class GildedRose {
         // si la qualité de l'item est en dessous de 50
         if (item.quality < QUALITE_MAX) {
             // on augmente sa qualité de 1
-            item.quality = item.quality + 1;
+            item.quality++;
         }
 
         if (item.sellIn < PRIX_MAX1) {
             if (item.quality < QUALITE_MAX) {
-                item.quality = item.quality + 1;
+                item.quality++;
             }
         }
 
         if (item.sellIn < PRIX_MAX2) {
             if (item.quality < QUALITE_MAX) {
-                item.quality = item.quality + 1;
+                item.quality++;
             }
         }
 
@@ -78,16 +78,16 @@ class GildedRose {
 
     private void updateQualityDefault(Item item) {
         if (item.quality > 0) {
-            item.quality = item.quality - 1;
+            item.quality--;
         }
 
-        item.sellIn = item.sellIn - 1;
+        item.sellIn--;
 
         // si le prix de l'item est en dessous de 0
         if (item.sellIn < 0) {
             // si l'item à une qualité supérieur à 0
             if (item.quality > 0) {
-                item.quality = item.quality - 1;
+                item.quality--;
             }
         }
     }
