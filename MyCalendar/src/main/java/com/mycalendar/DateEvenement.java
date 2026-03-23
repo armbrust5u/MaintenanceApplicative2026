646ca25aad8150abc54ee634c7bb6d4d45526a03
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2026-03-23 10:11:53 +0100

    feat: ajout de DateEvenement | les tests passent VERT

---
+++ MyCalendar/src/main/java/com/mycalendar/DateEvenement.java
@@ -0,0 +1,26 @@
+package com.mycalendar;
+
+import java.time.LocalDateTime;
+
+public record DateEvenement(LocalDateTime valeur) {
+    public DateEvenement {
+        if (valeur == null)
+            throw new IllegalArgumentException("La date ne peut pas être nulle");
+    }
+
+    public boolean estAvant(DateEvenement autre) {
+        return this.valeur.isBefore(autre.valeur);
+    }
+
+    public boolean estApres(DateEvenement autre) {
+        return this.valeur.isAfter(autre.valeur);
+    }
+
+    public DateEvenement plusMinutes(DureeEvenement duree) {
+        return new DateEvenement(this.valeur.plusMinutes(duree.minutes()));
+    }
+
+    public DateEvenement plusJours(FrequenceEvenement frequence) {
+        return new DateEvenement(this.valeur.plusDays(frequence.jours()));
+    }
+}
