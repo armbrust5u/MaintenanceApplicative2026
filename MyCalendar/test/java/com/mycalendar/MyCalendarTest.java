599667d48ea2c77199e3899af17e042093e0a0cc
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2026-03-23 09:50:24 +0100

    test: ajout de tests MyCalendar ROUGE

--- MyCalendar/test/java/com/mycalendar/MyCalendarTest.java
+++ MyCalendar/test/java/com/mycalendar/MyCalendarTest.java
@@ -11,4 +11,29 @@ class MyCalendarTest {
         assertThrows(IllegalArgumentException.class,
                 () -> new TitreEvenement(""));
     }
+
+    @Test
+    void dureeNegativeInvalide() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new DureeEvenement(-1));
+    }
+
+    @Test
+    void dureeZeroInvalide() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new DureeEvenement(0));
+    }
+
+    @Test
+    void frequenceNegativeInvalide() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new FrequenceEvenement(-1));
+    }
+
+    @Test
+    void eventIdUnique() {
+        EventId id1 = EventId.nouveau();
+        EventId id2 = EventId.nouveau();
+        assertNotEquals(id1, id2);
+    }
 }
