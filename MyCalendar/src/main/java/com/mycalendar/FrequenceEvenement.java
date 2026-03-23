5f253e7c025469e16c17e8a2df043b303573dc82
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2026-03-23 09:53:07 +0100

    ajout FrequenceEvenement | 1 tests passent VERT

---
+++ MyCalendar/src/main/java/com/mycalendar/FrequenceEvenement.java
@@ -0,0 +1,8 @@
+package com.mycalendar;
+
+public record FrequenceEvenement(int jours) {
+    public FrequenceEvenement {
+        if (jours <= 0)
+            throw new IllegalArgumentException("La fréquence doit être positive");
+    }
+}
